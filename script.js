915f1abc3933bb5814a00ac9153baefb98c0d5e0
Umer <muhammadumershabir@gmail.com>
2023-04-12 04:34:09 +0500

    add, edit, clear all

--- script.js
+++ script.js
@@ -2,19 +2,43 @@
 let addBtn = document.querySelector("#addItem");
 let textValue = document.querySelector("#item");
 let listing = document.querySelector(".list-items");
-console.log(textValue);
+let clearBtn = document.getElementById("clear-all");
+
 // functionalities
 // adding shoping item
 function addListItem(e) {
   e.preventDefault();
   let node = document.createElement("li");
-
+  let innerSpan = document.createElement("span");
+  let editBtn = document.createElement("button");
+  let editnote = document.createTextNode("Edit");
+  let deleteBtn = document.createElement("button");
+  let deleteNote = document.createTextNode("Delete");
+  deleteBtn.className = "delete-item";
+  deleteBtn.appendChild(deleteNote);
+  editBtn.appendChild(editnote);
+  innerSpan.appendChild(editBtn);
+  innerSpan.appendChild(deleteBtn);
   let innerNode = document.createTextNode(textValue.value);
+  node.className = "item";
   node.appendChild(innerNode);
+  node.appendChild(innerSpan);
   listing.appendChild(node);
+  textValue.value = "";
 }
 function inputValue(e) {
   return e.target.value;
 }
+function deleteItem(e) {
+  if (e.target.classList.contains("delete-item")) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+function clearAll(e) {
+  let childList = e.target.previousElementSibling;
+  childList.innerHTML = "";
+}
 // events
 addBtn.addEventListener("click", addListItem);
+listing.addEventListener("click", deleteItem);
+clearBtn.addEventListener("click", clearAll);
